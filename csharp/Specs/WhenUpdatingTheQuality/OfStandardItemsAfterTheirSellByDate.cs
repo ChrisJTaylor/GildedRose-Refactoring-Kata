ef400384c7f0bdf975fe42d827ea7783485f3e7f
Christian Taylor <christian.taylor@machinology.com>
2024-03-22 15:37:39 +0000

    add tests for standard items

---
+++ csharp/Specs/WhenUpdatingTheQuality/OfStandardItemsAfterTheirSellByDate.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace csharp.Specs.WhenUpdatingTheQuality;
+
+public class OfStandardItemsAfterTheirSellByDate
+{
+    private readonly IList<Item> _items = new List<Item>();
+    
+    [OneTimeSetUp]
+    public void BeforeAll()
+    {
+        _items.Add(new Item { Name = "Sword", SellIn = 0, Quality = 10 });
+        _items.Add(new Item { Name = "Shield", SellIn = 0, Quality = 10 });
+        _items.Add(new Item { Name = "Dagger", SellIn = 0, Quality = 10 });
+        
+        var app = new GildedRose(_items);
+        app.UpdateQuality();
+    }
+
+    [Test]
+    public void TheQualityShouldLowerByTwo()
+    {
+        _items.Should().AllSatisfy(item => item.Quality.Should().Be(8));
+    }
+    
+    [Test]
+    public void TheSellInDaysShouldLowerByOne()
+    {
+        _items.Should().AllSatisfy(item => item.SellIn.Should().Be(-1));
+    }
+}
---
+++ csharp/Specs/WhenUpdatingTheQuality/OfStandardItemsBeforeTheirSellByDate.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace csharp.Specs.WhenUpdatingTheQuality;
+
+public class OfStandardItemsBeforeTheirSellByDate
+{
+    private readonly IList<Item> _items = new List<Item>();
+    
+    [OneTimeSetUp]
+    public void BeforeAll()
+    {
+        _items.Add(new Item { Name = "Sword", SellIn = 5, Quality = 10 });
+        _items.Add(new Item { Name = "Shield", SellIn = 5, Quality = 10 });
+        _items.Add(new Item { Name = "Dagger", SellIn = 5, Quality = 10 });
+        
+        var app = new GildedRose(_items);
+        app.UpdateQuality();
+    }
+
+    [Test]
+    public void TheQualityShouldLowerByOne()
+    {
+        _items.Should().AllSatisfy(item => item.Quality.Should().Be(9));
+    }
+    
+    [Test]
+    public void TheSellInDaysShouldLowerByOne()
+    {
+        _items.Should().AllSatisfy(item => item.SellIn.Should().Be(4));
+    }
+}
