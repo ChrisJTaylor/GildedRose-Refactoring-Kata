50d6643c1acba918ddd2c0494777228b88197824
Christian Taylor <christian.taylor@machinology.com>
2024-03-22 16:08:28 +0000

    add case for sulfuras

---
+++ csharp/Specs/WhenUpdatingTheQuality/OfSulfuras/BeforeItsSellByDate.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace csharp.Specs.WhenUpdatingTheQuality.OfSulfuras;
+
+public class BeforeItsSellByDate
+{
+    private readonly IList<Item> _items = new List<Item>();
+    
+    [OneTimeSetUp]
+    public void BeforeAll()
+    {
+        _items.Add(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 10, Quality = 10 });
+        
+        var app = new GildedRose(_items);
+        app.UpdateQuality();
+    }
+
+    [Test]
+    public void TheQualityShouldNotChange()
+    {
+        _items.Should().AllSatisfy(item => item.Quality.Should().Be(10));
+    }
+    
+    [Test]
+    public void TheSellInDaysShouldNotChange()
+    {
+        _items.Should().AllSatisfy(item => item.SellIn.Should().Be(10));
+    }
+}
