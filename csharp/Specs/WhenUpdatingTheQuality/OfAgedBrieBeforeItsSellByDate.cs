b261c5f7c9cb1014ad6029326aa58f85b65cf1a0
Christian Taylor <christian.taylor@machinology.com>
2024-03-22 15:46:53 +0000

    add cases for aged brie

---
+++ csharp/Specs/WhenUpdatingTheQuality/OfAgedBrieBeforeItsSellByDate.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace csharp.Specs.WhenUpdatingTheQuality;
+
+public class OfAgedBrieBeforeItsSellByDate
+{
+    private readonly IList<Item> _items = new List<Item>();
+    
+    [OneTimeSetUp]
+    public void BeforeAll()
+    {
+        _items.Add(new Item { Name = "Aged Brie", SellIn = 5, Quality = 10 });
+        
+        var app = new GildedRose(_items);
+        app.UpdateQuality();
+    }
+
+    [Test]
+    public void TheQualityShouldIncreaseByOne()
+    {
+        _items.Should().AllSatisfy(item => item.Quality.Should().Be(11));
+    }
+    
+    [Test]
+    public void TheSellInDaysShouldLowerByOne()
+    {
+        _items.Should().AllSatisfy(item => item.SellIn.Should().Be(4));
+    }
+}
