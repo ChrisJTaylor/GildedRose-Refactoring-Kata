9feb642c182b4352ceeee29d078e6772a548d4cf
Christian Taylor <christian.taylor@machinology.com>
2024-03-24 10:44:51 +0000

    reset the Items property
    so the goblin doesn't get me

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -3,18 +3,18 @@ using Inventory.UpdateStrategies;
 
 public sealed class GildedRose
 {
-    private readonly IList<Item> _items;
+    IList<Item> Items;
     private readonly IUpdateItemStrategy[] _updateItemStrategies;
 
     public GildedRose(IList<Item> items, IUpdateItemStrategy[] updateItemStrategies)
     {
-        _items = items;
+        Items = items;
         _updateItemStrategies = updateItemStrategies;
     }
 
     public void UpdateQuality()
     {
-        foreach (var currentItem in _items)
+        foreach (var currentItem in Items)
         {
             foreach (var updateItemStrategy in _updateItemStrategies)
             {
